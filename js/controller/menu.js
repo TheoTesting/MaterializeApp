e13da77fbf2d914e74ce34758494244a159ebf9f
lheo21 <lheo21@outlook.com>
2017-01-28 23:59:32 -0500

    Creacion del cardpanel para el login de la app

--- js/controller/menu.js
+++ js/controller/menu.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
+  if (document.getElementById('index')) {
     $("#header").load("views/templates/menu.html");
+  }else {
+    $("#header").load("../views/templates/_menu.html");
+  }
 });
