d83dcf1baf26350dc5386b4c57ee73ec7d7fc286
TheoRev <hrevillafernandez79@gmail.com>
2017-01-29 22:47:49 -0500

    Ruta de menubar modificado

--- js/controller/menu.js
+++ js/controller/menu.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   if (document.getElementById('index')) {
     $("#header").load("views/templates/menu.html");
   }else {
-    $("#header").load("../views/templates/_menu.html");
+    $("#header").load("../views/templates/menu2.html");
   }
 });
